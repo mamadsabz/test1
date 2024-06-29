2033f96a247661b8586263f11577817fb024acf7
mamadsabz <164193238+mamadsabz@users.noreply.github.com>
2024-06-29 21:06:36 +0330

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,28 @@
-from django.urls import path
-from home_module.views import *
+"""
+URL configuration for test1 project.
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path , include
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('admin/', admin.site.urls),
+    path('products/', include('product_module.urls')),
+    path('home/', include('home_module.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
